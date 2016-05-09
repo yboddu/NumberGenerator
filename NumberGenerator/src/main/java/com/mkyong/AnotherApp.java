cd529a8cbbe87d78f323c6191dcd416cd0546132
yboddu <yogiboddu@gmail.com>
2016-05-09 21:52:47 +0100

    Second Commit

---
+++ NumberGenerator/src/main/java/com/mkyong/AnotherApp.java
@@ -0,0 +1,13 @@
+package com.mkyong;
+
+/**
+ * Hello world!
+ *
+ */
+public class AnotherApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World 2 !" );
+    }
+}
